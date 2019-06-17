f062e260c9adb844a7703bf2c25b14bab546f920
Sergey <43288911+s-yakubovskiy@users.noreply.github.com>
2019-06-17 10:08:57 +0300

    no tokens!

--- vk_bot/vk_bot.py
+++ vk_bot/vk_bot.py
@@ -10,7 +10,7 @@ from vk_api.longpoll import VkLongPoll, VkEventType
 
 
 WELCOME_MESSAGE = 'Welcome to Machine Can See *_*!\nSend a selfie to begin'
-KEYS = "95e00fddd8887f27bce737d4b90630b641d9c25b67f457ff0da194f8dd2df017d79af8d677ff0f33ffe11"
+KEYS = ""
 
 
 # Enable logging
